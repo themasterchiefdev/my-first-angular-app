55a3119305491e7517c87f3f24a71290a145fc83
CaptainChaos <yvrkarthik@gmail.com>
2019-06-18 22:12:08 +1200

    Update servers.component.ts
    Work completion as of video #19 - Specifying inline templates in the components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server> <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
