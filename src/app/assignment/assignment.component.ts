25cd36446cabc700953505692573d5dbf0f9d5e7
Rajiv Yanamandra <yvrkarthik@gmail.com>
2019-06-27 23:59:35 +1200

    assignment work
    Task-1,2 are complete

---
+++ src/app/assignment/assignment.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assignment',
+  templateUrl: './assignment.component.html',
+  styleUrls: ['./assignment.component.css']
+})
+export class AssignmentComponent implements OnInit {
+  buttonClicked = false;
+  constructor() {}
+
+  ngOnInit() {}
+}
