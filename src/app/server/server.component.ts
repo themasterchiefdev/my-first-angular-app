0e820a0dd76fe35ec3e52490c19d23452db7a7bb
Rajiv Yanamandra <yvrkarthik@gmail.com>
2019-06-27 21:23:22 +1200

    ngClass directive
    work completion as of video 38

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  // styleUrls: ['./server.component.css'],
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `
+  ]
 })
 export class ServerComponent implements OnInit {
   serverId = 10;
